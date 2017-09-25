15ee8a9b781c944e833dcc93ca7dd4daee48432f
Rolf Petersen <rolf3d@gmail.com>
2017-09-25 14:25:52 +0200

    Forbindelse til database og udskriv til consol.

--- AzureDatabase/Program.cs
+++ AzureDatabase/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,38 @@ namespace AzureDatabase
     {
         static void Main(string[] args)
         {
+            string query = "select * from Guest";
+
+            string con = "Data Source=hotelserverrolf.database.windows.net;Initial Catalog=HotelDB;Integrated Security=False;User ID=Rolles;Password=Holles28462846;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            using (SqlConnection connection = new SqlConnection(con))
+            {
+                connection.Open();
+                using (SqlCommand selectCommand = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int Guest_NO = reader.GetInt32(0);
+                            string Name = reader.GetString(1);
+                            string Address = reader.GetString(2);
+
+
+                            Console.WriteLine(Guest_NO + " " + Name + " " + Address);
+                            if (Name.Contains("Sten"))
+                            {
+                                Console.WriteLine("Hej " + Name);
+                                Console.ReadLine();
+                            }
+                        }
+                    }
+                }
+                Console.ReadLine();
+
+            }
+
+
         }
     }
 }
